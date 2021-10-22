0d2fbb71c75367b5d7da09f58f10b3f96e8147ea
denis.ten <denis.ten@aliexpress.ru>
2021-10-22 19:18:48 +0300

    created users module. added 2 endpoints (getAll, createUser)

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 8000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
-
+  await app.listen(port, () => console.log('app is running on port:', port));
 }
 bootstrap();
---
+++ src/users/index.ts
@@ -0,0 +1,4 @@
+export { UsersModel } from './users.model';
+export { UsersController } from './users.controller';
+export { UsersService } from './users.service';
+export { UsersModule } from './users.module';
